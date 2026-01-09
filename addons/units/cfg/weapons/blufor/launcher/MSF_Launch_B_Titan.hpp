91f179c4e36ad3c52ad5c62b557517ea13c1d31e
zeiktuvai <zeik.tuvaii@gmail.com>
2026-01-08 22:41:01 -0500

    Added titan AA launcher.

--- addons/units/cfg/weapons/blufor/launcher/MSF_Launch_B_Titan.hpp
+++ addons/units/cfg/weapons/blufor/launcher/MSF_Launch_B_Titan.hpp
@@ -3,5 +3,5 @@ class MSF_launch_B_Titan : launch_B_Titan_F
     displayName = "Titan MPRL (Alpine)";
     author = "Zeik_Tuvai";
     hiddenSelections[] = {"camo_launcher","camo_tube"};
-    hiddenSelectionsTextures[] = {"z\msf\addons\units\data\blufor\w\weapons\titan_launcher.paa","A3\Weapons_F_Beta\Launchers\Titan\Data\TubeL_CO.paa"};
+    hiddenSelectionsTextures[] = {"z\msf\addons\units\data\blufor\w\weapons\titan_launcher.paa","z\msf\addons\units\data\blufor\w\weapons\Tube_l.paa"};
 };
